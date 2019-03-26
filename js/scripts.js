d347d3c9b021e8274101d040772b675e2a083876
smn <48667271+sabrinamianne@users.noreply.github.com>
2019-03-25 21:13:43 -0700

    Created the last code for the Cipher homework.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,43 @@
 
 var string=prompt("What is yout favorite color? ");
 var jsF=function (string){
-    return string.charAt(0).toUpperCase ()+string.slice(1,-1)+string.slice(-1).toUpperCase();
+    return string.charAt(0).toUpperCase ()+string.slice(-1).toUpperCase();
 };
 alert(jsF(string))
+
 var str=prompt("What is your current job?");
+
 var func2=function(str){
-	var firstLetter=str.charAt(0);
-  var middleLetter=str.slice(1,-1);
+  var firstLetter=str.charAt(0);
   var lastLetter=str.slice(-1);
-  return lastLetter+middleLetter+firstLetter;
+  return lastLetter+firstLetter;
 };
 alert(func2(str))
 
 var str1=prompt("What is your hobby?");
-var func3=function(str1){
-	alert(jsF(str1)); 
-  alert(func2(str1));
-};
 
-alert(func3(str1))
+var func3=function(str1){
+  alert(jsF(str1))
+  var str=jsF(str1); 
+  alert(func2(str));
+  var result= func2(str);
+  alert(str1.concat(result))
+};
+alert(func3(str1));
+
+var func4=function (string){
+  var nb=Math.round(string.length);
+  var nb1=nb/2;
+  var le=string.charAt(nb1);
+  var str=jsF(string); 
+  var result= func2(str);
+  return le+string+result
+};
+alert(func4(string));
+
+var rev=function (string){
+  var strR=func4(string);
+  return strR.split("").reverse().join("")
+}
+alert(rev(string))
+
